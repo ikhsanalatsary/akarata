421a1e8a752e42309ddbca003077f2eb7bd982bb
ikhsanalatsary <ikhsannetwork@gmail.com>
2019-04-18 18:44:46 +0700

    fix(): support multiple words correctly

--- src/stemmer.ts
+++ src/stemmer.ts
@@ -4,7 +4,10 @@ class Stemmer extends MorphologicalUtility {
   stem = (word: string, derivationalStemming = true) => {
     this.flags = undefined
     if (word.match(/\s/)) {
-      word.split(' ').map((w) => this.stem(w))
+      word = word
+        .split(/[,\n.\s+]+/)
+        .map((w) => this.stem(w.trim().toLowerCase()))
+        .join(', ')
     } else {
       this.numberOfSyllables = this.totalSyllables(word)
       if (this.isStillHasManySyllables()) {
